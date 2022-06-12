fb40dd36af864d9a18e4b3ce09da2bb02d02b4fd
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2022-06-12 22:35:40 +0300

    Create ghul.swift

---
+++ ghul.swift
@@ -0,0 +1,13 @@
+var a = 1000
+var b = 7
+while a > b {
+	var c = a - b
+	print("\(a) - \(b) = \(c)")
+	a = c
+  //В swift для windows почему то не работает sleep, пришлось искать обходной путь)
+	var d = 100000000
+	while d != 0 {
+		d -= 1
+	}
+}
+print("I'm Ghul!")
