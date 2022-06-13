65ed2bf01943c70b0583f80f983f9efb37f5c084
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2022-06-13 13:11:09 +0300

    Update ghul.swift

--- ghul.swift
+++ ghul.swift
@@ -4,7 +4,7 @@ while a > b {
 	var c = a - b
 	print("\(a) - \(b) = \(c)")
 	a = c
-  //В swift для windows почему то не работает sleep, пришлось искать обходной путь)
+  //В swift для windows почему-то не работает sleep, пришлось искать обходной путь)
 	var d = 100000000
 	while d != 0 {
 		d -= 1
